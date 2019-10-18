50ca9ef8c88ccddfa57b5f7505638b36156e6e39
Soumya Bastikar <bastikarsoumya95@gmail.com>
2019-10-18 08:35:06 +0530

    Added Body-parser,Morgan(logging)

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -17,16 +17,24 @@ router.delete('/:orderId', (req, res, next) => {
 });
 
 router.post('/:orderId', (req, res, next) => {
+
     const id = req.params.orderId;
-    if(id === 'special') {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    };
+
+    if (id === 'special') {
         res.status(200).json({
-            message: 'Handling special DELETE requests to /orders/id',
-            id: id
+            message: 'Handling special POST requests to /orders/id',
+            id: id,
+            order: order
         });
     } else {
         res.status(200).json({
-            message: 'Handling DELETE requests to /orders/id',
-            id: id
+            message: 'Handling POST requests to /orders/id',
+            id: id,
+            order: order
         });
     }
 });
--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
@@ -9,9 +12,28 @@ const orderRoutes = require('./api/routes/orders');
 //     });
 // });
 
+//logging
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //Routes handling requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
